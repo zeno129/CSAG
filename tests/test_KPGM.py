fd2943d63e6ac0b1d094753d38a4976d0b75baa8
Giselle Zeno <moi@gisellezeno.com>
2018-01-08 16:01:04 -0500

    first test!

--- tests/test_KPGM.py
+++ tests/test_KPGM.py
@@ -18,22 +18,28 @@ from kronecker import KPGM as model
 #     assert (1,2) in g.edges
 #     assert (2,1) in g.edges
 
-def test_random_probability():
+def test_equal_probabilities():
     b = 2
     theta = [[0.5, 0.5], [0.5, 0.5]]
     k = 2
     vertices = range(operator.pow(b, k))
-    # possible_edges = list(itertools.combinations_with_replacement(vertices, 2))
+    n = 100
+
     possible_edges = list(itertools.product(vertices, repeat=2))
-    # counts = defaultdict(int)
     counts = {edge: 0 for edge in possible_edges}
 
-    for i in range(10):
+    for i in range(n):
         g = model.KPGM(theta, k, b)
         for e in possible_edges:
             if e in g.edges:
                 counts[e] += 1
 
-    sys.stdout.write(str(counts))
+    # sys.stdout.write(str(counts))
 
-    assert True
+    p = 0.25
+    mean = n * p
+    std_dev = n * p * (1 - p)
+
+    for cnt in counts.values():
+        assert cnt >= mean - std_dev
+        assert cnt <= mean + std_dev
