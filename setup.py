f13abdb6aa01d91a211d55a6fb1eee364ff4bc07
Giselle Zeno <moi@gisellezeno.com>
2018-02-01 12:16:27 -0500

    add other packages

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 setup(name='graph_sampling',
       version='0.1',
       package_dir={'graph_sampling': ''},
-      packages=['graph_sampling.kronecker'],  # add rest here
+      packages=['graph_sampling.kronecker', 'graph_sampling.graph'],  # add rest here
       )
